4083be6db9a3ad575e508c1c6f934c08d39c48aa
nel11326 <lenor11326@hotmail.com>
2024-08-21 23:46:36 +0800

    projects(create): create a projects function

--- src/components/Projects/ProjectCard.jsx
+++ src/components/Projects/ProjectCard.jsx
@@ -12,30 +12,34 @@ export const ProjectCard = ({
 }) => {
   return (
     <div className={styles.container}>
-      <img
-        src={getImageUrl(imageSrc)}
-        alt={`Image of ${title}`}
-        className={styles.image}
-      />
-      <h3 className={styles.title}>{title}</h3>
-      <p className={styles.description}>{description}</p>
-      <ul className={styles.skills}>
-        {skills.map((skill, id) => {
-          return (
-            <li key={id} className={styles.skill}>
-              {skill}
-            </li>
-          );
-        })}
-      </ul>
-      <div className={styles.links}>
-        <a href={source} className={styles.link}>
-          code <img />
-        </a>
-        <a href={demo} className={styles.link}>
-          Live Demo
-          <img />
-        </a>
+      <div>
+        <img
+          src={getImageUrl(imageSrc)}
+          alt={`Image of ${title}`}
+          className={styles.image}
+        />
+      </div>
+      <div>
+        <h3 className={styles.title}>{title}</h3>
+        <p className={styles.description}>{description}</p>
+        <ul className={styles.skills}>
+          {skills.map((skill, id) => {
+            return (
+              <li key={id} className={styles.skill}>
+                {skill}
+              </li>
+            );
+          })}
+        </ul>
+        <div className={styles.links}>
+          <a href={source} className={styles.link}>
+            code <img />
+          </a>
+          <a href={demo} className={styles.link}>
+            Live Demo
+            <img />
+          </a>
+        </div>
       </div>
     </div>
   );
--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,3 +1,19 @@
-function Projects() {}
+import styles from "./Projects.module.css";
+
+import projects from "../../Data/projects.json";
+import { ProjectCard } from "../Projects/ProjectCard";
+
+function Projects() {
+  return (
+    <section className={styles.container} id="projects">
+      <h2 className={styles.title}>Projects</h2>
+      <div className={styles.projects}>
+        {projects.map((project, id) => (
+          <ProjectCard key={id} {...project} />
+        ))}
+      </div>
+    </section>
+  );
+}
 
 export default Projects;
